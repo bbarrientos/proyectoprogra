0a49308099335ab30ce83754a9d208b2ecea7b65
Colonello <xerman12@gmail.com>
2018-07-12 22:39:13 -0400

    Prototipo CODA app 01
    Primer prototipo de la aplicacion movil...

---
+++ lib/views/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text('CODA UFRO'),),
+      body: new Center(
+        child:  Container(
+          padding: new EdgeInsets.all(32.0),
+          child: new Column(
+            children: <Widget>[
+              new Image.asset('images/ufro.png',width: 150.0,height: 150.0,),
+              new RaisedButton(
+                elevation: (5.0),
+                child: new Text("Log in"),
+                onPressed: (){Navigator.of(context).pushNamed('/Login');}
+              )
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
